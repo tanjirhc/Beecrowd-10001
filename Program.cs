a871b8c90b81f60215c341b15ea874ec9c8a14d8
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-08 00:49:30 +0600

    Another way to read input by Parse & Trim()

--- Program.cs
+++ Program.cs
@@ -10,15 +10,11 @@ namespace Beecrowd_10001
     {
         static void Main(string[] args)
         {
-            int x;
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-
-            x = a + b;
-            Console.WriteLine($"X = {x} ");
-
-            Console.ReadKey();
-
+            int A = Int32.Parse(System.Console.ReadLine().Trim());
+            int B = Int32.Parse(System.Console.ReadLine().Trim());
+            int X = A + B;
+            Console.Write("X = {0}\n", X);
+            Console.ReadLine();
         }
     }
 }
